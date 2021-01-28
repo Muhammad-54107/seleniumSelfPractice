eb71f1a53e5c332b2955a6c6222a0f8e677f5202
Muhammad-54107 <Muhammad.qureshi107@gmail.com>
2021-01-27 20:21:53 -0500

    added new changes

--- src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
+++ src/test/java/com/cybertek/tests/day8_testbase_properties_driver/WebTablePractices.java
@@ -2,11 +2,6 @@ package com.cybertek.tests.day8_testbase_properties_driver;
 
 public class WebTablePractices {
 
-    //PRACTICE #4: Method: verifyOrder
-    // •Create a method named verifyOrder in SmartBearUtils class.
-    // •Method takes WebDriver object and String(name).
-    // •Method should verify if given name exists in orders.
-    // •This method should simply accepts a name(String), and assert whether given name is in the list or not.
-    // •Create a new TestNG test to test if the method is working as expected.
+
 
 }
---
+++ src/test/java/com/cybertek/tests/utilites/WebOrderUtils.java
@@ -0,0 +1,37 @@
+package com.cybertek.tests.utilites;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WebOrderUtils {
+
+    /*
+       Make the method static so that we dont have to create
+       instance of the class.
+    */
+    public static void loginToSmartBear(WebDriver driver){
+        //3. Enter username: “Tester”
+        WebElement inputUsername = driver.findElement(By.id("ctl00_MainContent_username"));
+        inputUsername.sendKeys("Tester");
+
+        //4. Enter password: “test”
+        WebElement inputPassword = driver.findElement(By.id("ctl00_MainContent_password"));
+        inputPassword.sendKeys("test");
+
+        //5. Click to Login button
+        WebElement loginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
+        loginButton.click();
+    }
+
+    //PRACTICE #4: Method: verifyOrder
+    // •Create a method named verifyOrder in SmartBearUtils class.
+    // •Method takes WebDriver object and String(name).
+    // •Method should verify if given name exists in orders.
+    // •This method should simply accepts a name(String), and assert whether
+    // given name is in the list or not.
+    // •Create a new TestNG test to test if the method is working as expected.
+
+
+
+}
